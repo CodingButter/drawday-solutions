3768bfafc0bcf956f495de2e44894babcbea4c47
Jamie Nichols <JamieNichols@codingbutter.com>
2025-08-27 01:46:03 -0400

    Update to production domain drawday.app
    - Updated NEXT_PUBLIC_APP_URL environment variable in Vercel
    - Updated extension iframe URLs to use https://drawday.app
    - Rebuilt extension with production domain
    - Ready for production deployment

--- apps/extension/src/sidepanel-iframe.tsx
+++ apps/extension/src/sidepanel-iframe.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app.css';
 
-// Get the website URL - for now always use localhost for development
-// TODO: Update this to use production URL when deploying to Chrome Web Store
-const WEBSITE_URL = 'http://localhost:3000';
+// Get the website URL - using production domain
+const WEBSITE_URL = 'https://drawday.app';
 
 function SidePanelIframe() {
   return (
